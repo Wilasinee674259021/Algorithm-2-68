fe8f4810e21b18b5d656e6d3f88ab57b711acf08
Your NameWilasinee674259021 <you@example.com674259021@webmail.npru.ac.th>
2025-07-25 15:25:43 +0700

    Change input type to keyboard

--- Sum.java
+++ Sum.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class Sum {
     public static void main(String[] args) {
-        int num1 = 4;
-        int num2 = 6;
+        // int num1 = 4;
+        // int num2 = 6;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter num1 ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter num2 ");
+        int num2 = scanner.nextInt();
+
         int sum = num1 + num2;
         System.out.print("sum of " + num1 + " and " + num2 + " = " + sum);
     }
